f796a41c92a9bd52574782a2762b1de8db5c47a9
Thomas Neustupny <thn@tigris.org>
2010-03-05 09:05:44 +0000

    preparing for release 0.30
    git-svn-id: http://argouml-java.tigris.org/svn/argouml-java/trunk@285 d207366d-db1d-0410-bd97-b94953444b2d

--- src/org/argouml/language/java/JavaModuleGlobals.java
+++ src/org/argouml/language/java/JavaModuleGlobals.java
@@ -48,7 +48,7 @@ public final class JavaModuleGlobals {
     /**
      * The version info of a module in the Java module project.
      */
-    public final static String MODULE_VERSION = "0.30.ALPHA_2";
+    public final static String MODULE_VERSION = "0.30";
 
     /**
      * The author info of a module in the Java module project.
