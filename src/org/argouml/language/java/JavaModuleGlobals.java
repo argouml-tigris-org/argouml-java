7913496fe34301cef8eee29f8693f30b7ca1dbe2
Thomas Neustupny <thn@tigris.org>
2010-12-03 09:15:59 +0000

    updating module version info
    git-svn-id: http://argouml-java.tigris.org/svn/argouml-java/trunk@362 d207366d-db1d-0410-bd97-b94953444b2d

--- src/org/argouml/language/java/JavaModuleGlobals.java
+++ src/org/argouml/language/java/JavaModuleGlobals.java
@@ -48,7 +48,7 @@ public final class JavaModuleGlobals {
     /**
      * The version info of a module in the Java module project.
      */
-    public static final String MODULE_VERSION = "0.30.2";
+    public static final String MODULE_VERSION = "0.31.8";
 
     /**
      * The author info of a module in the Java module project.
